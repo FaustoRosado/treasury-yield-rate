f16f2def27f06fea9413b773ae0c1af7c94eeef6
FaustoRosado <faustrosado@gmail.com>
2021-05-02 23:21:49 -0500

    scraped and parsed data

--- script.py
+++ script.py
@@ -7,8 +7,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 import requests
 
-def scrape_data():
+url = 'https://www.treasury.gov/resource-center/data-chart-center/interest-rates/Pages/TextView.aspx?data=yield'
+
+def scrape_data(url):
     print("scraping")
+    
+    yield_curve = pd.read_html(url, parse_dates = True) 
+    yield_curve = yield_curve[1].dropna(how = 'any')
+
+    yield_curve_10_2 = yield_curve.iloc[:,[0,6,10]]
+    yield_curve_10_2.columns = ['date', '2 yr', '10 yr']
+
+    yield_curve.iloc[:,[6,10]] = yield_curve_10_2[['2 yr', '10 yr']].astype(float)
+
+    yield_curve_10_2['10yr - 2yr'] = yield_curve_10_2['10 yr'] - yield_curve_10_2['2 yr']
+
+    return yield_curve_10_2
     #req = requests.get("https://www.treasury.gov/resource-center/data-chart-center/interest-rates/Pages/TextView.aspx?data=yield")
     #soup = bs(req.text)
 
